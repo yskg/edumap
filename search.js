47dd7dd711ebe8e557b2a7d0946f6b4ab7519503
yskg <zhujieyyzq@126.com>
2016-08-04 21:41:22 +0800

    Add files via upload

---
+++ search.js
@@ -0,0 +1,91 @@
+﻿
+var search_execute, clear_search, position_Feature;
+var search_symbol;
+var search_url,search_field;
+var search_layerid = new Array();
+
+var table_execute;//用于高亮选择当前表格行数据
+
+//根据输入的关键字进行findTask操作
+require([
+    "esri/tasks/FindTask",
+    "esri/tasks/FindParameters",
+   "esri/symbols/PictureMarkerSymbol",
+   "esri/geometry/Point",
+   "esri/InfoTemplate"
+], function (FindTask, FindParameters, PictureMarkerSymbol, Point, InfoTemplate)
+{
+       search_execute=function (searchtext) {
+        map.infoWindow.hide();
+        //初始化查询
+        var findTask = new FindTask(search_url);
+        search_symbol = new PictureMarkerSymbol('school_image/location.png', 20, 20);
+        //new esri.symbol.SimpleMarkerSymbol(esri.symbol.SimpleMarkerSymbol.STYLE_SQUARE, 10, new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID, new dojo.Color([255, 0, 0]), 1), new dojo.Color([0, 255, 0, 0.25]));
+        findParams.searchText =searchtext;
+        findTask.execute(findParams, search_Results);
+    }
+
+    function search_Results(results) {
+        map.graphics.clear();
+        var innerHtml = "";
+        var symbol = search_symbol;
+        for (var i = 0; i < results.length; i++) {
+            var curFeature = results[i];
+            var graphic = curFeature.feature;
+            graphic.setSymbol(symbol);
+        
+            map.graphics.add(graphic);
+            innerHtml += "<img src='school_image/location.png'/><a class='itemname' href='javascript:position_Feature(" + graphic.attributes.FID + ")'>" + graphic.attributes.学校名称 + "</a><br>" +"<span class='itemaddress'>"+ graphic.attributes.校区+"</span>" + "<br/>";
+        }
+        document.getElementById("search_results").style.display = "block";
+        dojo.byId("search_items").innerHTML = innerHtml;
+    }
+
+    position_Feature =function(id) {
+        var sGrapphic;
+        //遍历地图的图形查找FID和点击行的FID相同的图形
+        for (var i = 0; i < map.graphics.graphics.length; i++) {
+            var cGrapphic = map.graphics.graphics[i];
+            if ((cGrapphic.attributes) && cGrapphic.attributes.FID == id) {
+                sGrapphic = cGrapphic;
+                break;
+            }
+        }
+        var sGeometry = sGrapphic.geometry;
+        // 当点击的名称对应的图形为点类型时进行地图中心定位显示
+        map.centerAndZoom(sGeometry,12);
+     //  var  screenPoint= map.toScreen(sGeometry);  //p是一个对象包括了.x/.y获得投影坐标
+        var iw = map.infoWindow;
+     
+    }
+    clear_search =function() {
+        map.graphics.clear();
+        map.infoWindow.hide();
+        document.getElementById("search_results").style.display = "none";
+    }
+
+    table_execute = function (searchtext) {
+        map.infoWindow.hide();
+        //初始化查询
+        var findTask = new FindTask(search_url);
+        findParams = new FindParameters();
+      
+        findParams.contains = false;//严格匹配输入的字段
+        search_symbol = new PictureMarkerSymbol('school_image/table_search.gif', 20, 20);
+        //new esri.symbol.SimpleMarkerSymbol(esri.symbol.SimpleMarkerSymbol.STYLE_SQUARE, 10, new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID, new dojo.Color([255, 0, 0]), 1), new dojo.Color([0, 255, 0, 0.25]));
+        findTask.execute(findParams, search_Results1);
+    }
+
+    function search_Results1(results) {
+        map.graphics.clear();
+        var symbol = search_symbol;
+        for (var i = 0; i < results.length; i++) {
+            var curFeature = results[i];
+           
+            infoTemplate.setContent("校区:${校区}<br/>学校地址:${地理位置}<br/>学校类型:${办学层次}<br/>联系电话:${联系电话}<br/>网址:${校园门户网}<br/>");
+            graphic.setInfoTemplate(infoTemplate);
+            map.graphics.add(graphic);
+        }
+    }
+
+})
---
+++ timelayer.js
@@ -0,0 +1,110 @@
+﻿
+
+//加载时间图层
+var time_info;
+var time_layer = new Array();
+var time_url = new Array();
+ time_url[0] = null;//985时间图层
+ time_url[1] = null;//211时间图层
+ time_url[2] = null;//本科时间图层
+ time_url[3] = null;//专科时间图层
+require([
+  "esri/toolbars/draw",
+  "esri/symbols/SimpleFillSymbol",
+  "esri/InfoTemplate",
+  "esri/layers/ArcGISDynamicMapServiceLayer",
+  "esri/TimeExtent",
+  "esri/dijit/TimeSlider",
+  "dojo/_base/array"], function (Draw, SimpleFillSymbol, SimpleLineSymbol, SimpleMarkerSymbol, Color, Graphic, InfoTemplate,
+    ArcGISDynamicMapServiceLayer, TimeExtent, TimeSlider, arrayUtils)
+{
+    var timeSlider;
+    time_info = function () {
+        var popupTemplate = new InfoTemplate("${学校名称}", "校区:${校区}<br/>地理位置:${地理位置}<br/>办学类型:${办学层次}<br/>联系电话:${联系电话}<br/>网址:${网址}<br/>");
+        for (i =3; i>= 0; i--) {
+            time_layer[i] = new ArcGISDynamicMapServiceLayer(time_url[i]);
+            time_layer[i].setInfoTemplates({
+                0: { infoTemplate: popupTemplate }
+            });
+            map.addLayer(time_layer[i]);//时间控制图层
+            time_layer[i].hide();
+        }
+        timeSlider = new TimeSlider({
+            style: "width: 100%;"
+        },dojo.byId("time_slider"));
+        map.setTimeSlider(timeSlider);
+        // timeSlider.play();//开始播放
+        //timeSlider.loop = true;//循环播放
+      
+        timeSlider.setThumbMovingRate(1000);//播放速度
+        timeSlider.startup();
+        var labels = arrayUtils.map(timeSlider.timeStops, function (timeStop, i) {
+            if (i %1 === 0) {//数字除以几下标时间间隔就是几
+                return timeStop.getUTCFullYear();
+            } else {
+                return "";
+            }
+        });
+        timeSlider.setLabels(labels);//用来设置时间轴下的时间数字
+        timeSlider.on("time-extent-change", function (evt) {
+            myevent = evt;
+            var endValString = evt.endTime.getUTCFullYear();
+            dojo.byId("date_range").innerHTML = "<i>" + endValString + "年<\/i>";
+        });
+    }
+})
+
+//打开时间轴
+var time_line = true;
+function turn_timecontrol() {
+    if (time_line) {
+       
+        time_line = false;
+    }
+    else {
+        document.getElementById("time_control").style.visibility = "hidden";
+        document.getElementById("time_info").style.visibility = "hidden";
+        time_line = true;
+    }
+}
+function close_timecontrol() {
+    document.getElementById("time_control").style.visibility = "hidden";
+
+}
+function close_timeline() {
+    document.getElementById("time_info").style.visibility = "hidden";
+    map.infoWindow.hide();
+    for (i = 0; i < time_layer.length; i++) {
+        time_layer[i].hide();
+    }
+    document.getElementById("time_985").checked = false;
+    document.getElementById("time_211").checked = false;
+    document.getElementById("time_bk").checked = false;
+    document.getElementById("time_zk").checked = false;
+    //timeSlider.destroy();摧毁小部件
+}
+function timeline_control(para) {
+    switch (para) {
+        case "time_985":
+            if (dojo.byId(para).checked) {
+                time_layer[0].show();
+                document.getElementById("time_info").style.visibility = "visible";
+            }
+   
+        case "time_bk":
+            if (dojo.byId(para).checked) {
+                time_layer[2].show();
+                document.getElementById("time_info").style.visibility = "visible";
+            }
+            else time_layer[2].hide();
+            break;
+        case "time_zk":
+            if (dojo.byId(para).checked) {
+                time_layer[3].show();
+                document.getElementById("time_info").style.visibility = "visible";
+            }
+            else time_layer[3].hide();
+            break;
+    }
+
+}
